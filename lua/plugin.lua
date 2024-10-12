c6e267b6b2cca46722e81eeff2a7a0ed3fcb9aee
zhangshurong <zhangshurong@bytedance.com>
2024-10-12 15:00:10 +0800

    init

--- lua/plugin.lua
+++ lua/plugin.lua
@@ -1,7 +1,11 @@
 local M = {}
+local log = vim.log.levels
 
+function M.debug_info(message)
+	vim.api.nvim_echo({ { message, "Info" } }, true, {})
+end
 function M.format()
-	print("Starting the formatting process...")
+	M.debug_info("Starting the formatting process...")
 
 	local clang_format_config = [[
 {
@@ -15,19 +19,19 @@ function M.format()
 	local path = vim.fn.tempname() .. ".clang-format"
 	vim.fn.writefile(vim.split(clang_format_config, "\n"), path)
 
-	print("Configuration file created at: " .. path)
-	print("Executing clang-format...")
+	M.debug_info("Configuration file created at: " .. path)
+	M.debug_info("Executing clang-format...")
 
 	vim.api.nvim_command("silent %!clang-format --style=" .. path)
 
 	if vim.v.shell_error ~= 0 then
-		print("Error during formatting.")
+		M.debug_info("Error during formatting.")
 	else
-		print("Formatting completed successfully.")
+		M.debug_info("Formatting completed successfully.")
 	end
 
 	vim.fn.delete(path)
-	print("Temporary files cleaned up.")
+	M.debug_info("Temporary files cleaned up.")
 end
 
 return M
